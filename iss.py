1929cc9f185a67a0ad4588dd07707ebe9cb32685
Kathryn Anderson <kat42272@yahoo.com>
2020-07-14 13:43:08 -0700

    Add function to retrieve time station passes over Indy

--- iss.py
+++ iss.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__author__ = "Kathryn Anderson"
+__author__ = "Kathryn Anderson with help from private tutor"
 
 import requests
 import turtle
@@ -18,6 +18,14 @@ def iss_position():
     retrieve = requests.get('http://api.open-notify.org/iss-now.json')
     location_of_station = retrieve.json()
     return location_of_station
+
+
+def time_in_indy():
+    retrieve = requests.get(
+        'http://api.open-notify.org/iss-pass.json?lat=39.768&lon=86.158')
+    flight_time = retrieve.json()
+    return time.ctime(flight_time["response"][1]["risetime"])
+
 
 
 def main():
