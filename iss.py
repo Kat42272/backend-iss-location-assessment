4b1069592296359ce17789d7a0eb5011e8c98431
Kathryn Anderson <kat42272@yahoo.com>
2020-07-14 14:01:20 -0700

    Add function that displays map and draws position of ISS

--- iss.py
+++ iss.py
@@ -7,6 +7,7 @@ import turtle
 import time
 
 def people_in_space():
+    # Retrieves list of people in space and their craft
     retrieve = requests.get('http://api.open-notify.org/astros.json')
     space = retrieve.json()
     for person in space["people"]:
@@ -15,16 +16,46 @@ def people_in_space():
 
 
 def iss_position():
+    # Retrieves location of ISS
     retrieve = requests.get('http://api.open-notify.org/iss-now.json')
     location_of_station = retrieve.json()
     return location_of_station
 
 
 def time_in_indy():
+    # Retrieves flight time of ISS over specific Lat/Long Coordinates
     retrieve = requests.get(
         'http://api.open-notify.org/iss-pass.json?lat=39.768&lon=86.158')
     flight_time = retrieve.json()
     return time.ctime(flight_time["response"][1]["risetime"])
+
+
+def space_turtle():
+    # Screen displaying the World Map and ISS 
+    display = turtle.Screen()
+    display.setup(600, 400)
+    display.bgpic("./map.gif")
+    display.reset()
+    display.setworldcoordinates(-170, -85, 180, 90)
+    display.addshape("iss.gif")
+    display.title("ISS Position Locator")
+
+    # Creates turtle
+    turtle.shape("iss.gif")
+    turtle.penup()
+    turtle.goto(-65, 70)
+    turtle.color("green")
+    turtle.write(time_in_indy(), font=("Arial", 15, "bold"))
+    turtle.goto(float(86.158), float(39.768))
+    turtle.dot(5, 'red')
+
+    while True:
+        iss = iss_position()
+        position = iss["iss_position"]
+        turtle.goto(float(position["longitude"]), float(position["latitude"]))
+        time.sleep(10)
+
+
 
 
 
