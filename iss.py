03cd0cefe9071ee6d2956db73d013816d278abb2
Kathryn Anderson <kat42272@yahoo.com>
2020-07-14 13:31:43 -0700

    Added function to retrieve list of people in space

--- iss.py
+++ iss.py
@@ -2,6 +2,17 @@
 
 __author__ = "Kathryn Anderson"
 
+import requests
+import turtle
+import time
+
+def people_in_space():
+    retrieve = requests.get('http://api.open-notify.org/astros.json')
+    space = retrieve.json()
+    for person in space["people"]:
+        print(person["name"] + ",", person["craft"])
+    print(f"{person['number']} people in space")
+
 
 
 
